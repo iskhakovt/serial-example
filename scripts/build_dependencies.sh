8c4863a15a54d9866bf6908df2d8401b39393a01
Timur Iskhakov <iskhakovt@gmail.com>
2021-02-21 01:45:53 +0000

    scripts

--- scripts/build_dependencies.sh
+++ scripts/build_dependencies.sh
@@ -2,11 +2,9 @@
 
 set -euo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 # serial
-mkdir -p "$DIR/serial/build"
-cd "$DIR/serial/build"
+mkdir -p "dependencies/serial/build"
+cd "dependencies/serial/build"
 cmake -DDISABLE_CATKIN=ON -DCATKIN_ENABLE_TESTING=OFF ..
 make
 sudo make install
